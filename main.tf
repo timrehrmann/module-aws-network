4e3bff9b1b5aa61ebcb15410a999a18e01a63b54
Tim Rehrmann <timsnebenmail@gmail.com>
2024-12-11 15:06:10 +0100

    update terraform to domain

--- main.tf
+++ main.tf
@@ -111,14 +111,14 @@ resource "aws_route_table_association" "public-b-association" {
 }
 
 resource "aws_eip" "nat-a" {
-  vpc = true
+  domain = "vpc"
   tags = {
     "Name" = "${local.vpc_name}-NAT-a"
   }
 }
 
 resource "aws_eip" "nat-b" {
-  vpc = true
+  domain = "vpc"
   tags = {
     "Name" = "${local.vpc_name}-NAT-b"
   }
